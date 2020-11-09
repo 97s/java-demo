9df2f16de5d3c8f1712d4b14228a798a90e33e81
chenzhongrun <chenzhongrun@chenzhongrundeMacBook-Pro.local>
2020-11-09 15:08:56 +0800

    feature: 增加方法

--- src/main/java/com/levo/dockerexample/controller/HelloController.java
+++ src/main/java/com/levo/dockerexample/controller/HelloController.java
@@ -12,7 +12,23 @@ public class HelloController {
 	
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String test() {
-		return "docker-java-app is up and running: " + new Date();
+		return "{\"info\": \"接口调用测试服务\", \"method\": \"GET\", \"success\": true, \"time\": " + new Date() + ", \"by\": \"chenzhongrun\"}";
 	}
 
+	@RequestMapping(value = "/test", method = RequestMethod.POST)
+	public String testPost() {
+		return "{\"info\": \"接口调用测试服务\", \"method\": \"POST\", \"success\": true, \"time\": " + new Date() + ", \"by\": \"chenzhongrun\"}";
+	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.PUT)
+	public String tesPut() {
+		return "{\"info\": \"接口调用测试服务\", \"method\": \"PUT\", \"success\": true, \"time\": " + new Date() + ", \"by\": \"chenzhongrun\"}";
+	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.DELETE)
+	public String testDelete() {
+		return "{\"info\": \"接口调用测试服务\", \"method\": \"DELETE\", \"success\": true, \"time\": " + new Date() + ", \"by\": \"chenzhongrun\"}";
+	}
 }
+
+
